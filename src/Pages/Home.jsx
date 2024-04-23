4b28a39fdc9180bd65ef947edd8201b75954fb22
Sameer Faridi <s.faridi007@gmail.com>
2024-04-23 22:35:28 +0530

    basic 2 page done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [num_0f_slots, setNum_0f_slots] = useState("");
 
-export default Home
+  const setNumber = e => {
+    const newValue = +e.target.value; // i know that "+" already {no use of GPT or google here}
+    if (newValue >= 0) {
+      setNum_0f_slots(newValue);
+      localStorage.setItem("num_0f_slots", newValue);
+      console.log(num_0f_slots);
+    }
+  };
+//   const isButton_desabled = num_0f_slots.trim() === "0"
+
+  return (
+    <div>
+      <h1 className="h1_home home_values">Please enter the number of slots</h1>
+      <div className="flex-col">
+        <input
+          className="Input_home home_values"
+          placeholder="69"
+          value={num_0f_slots}
+          onChange={setNumber}
+          type="number"
+        />
+        <Link to={`/parking`}>
+          <button /* disabled={isButtonDisabled} */ className="button">Next</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/Pages/ParkingGrid.jsx
+++ src/Pages/ParkingGrid.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import "../App.css";
+import React, { useState } from "react";
 
 const ParkingGrid = () => {
-  return (
-    <div>ParkingGrid</div>
-  )
-}
+  const [V_num, setV_num] = useState("");
+  const setVehicleNumber = e => {
+    const newValue = +e.target.value;
+        setV_num(newValue);
+      localStorage.setItem("V_num", V_num);
+      console.log(V_num);
+  };
 
-export default ParkingGrid
+  const num_0f_slots = localStorage.getItem("num_0f_slots");
+  console.log(num_0f_slots);
+  return (
+    <div>
+      <div>
+        <input
+          className="Input_home home_values"
+          placeholder="69"
+          onChange={setVehicleNumber}
+          type="text"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ParkingGrid;
